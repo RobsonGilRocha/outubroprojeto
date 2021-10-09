6238a95c6cdacbc24aaf05d881b7587695bb511b
Robson Gil Rocha <gil1990gil@hotmail.com>
2021-10-09 14:26:43 -0300

    paginação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import {Login, Home, Header,Footer,Contato,Catalog, Wallpaper } from './components'
-import {useSelector, useDispatch} from 'react-redux'
+import {BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
+//import {Login, Home, Header,Footer,Contato,Catalog, Wallpaper } from './components'
+//import {useSelector, useDispatch} from 'react-redux'
 
 function App() {
-  const page = useSelector( state => state.page);
-  const dispatch = useDispatch();
+ // const page = useSelector( state => state.page);
+ // const dispatch = useDispatch();
 
-  function PAGEZERO (){
+  /* function PAGEZERO (){
     dispatch({ type: 'PAGE_[0]'})
   }
   function PAGEONE (){
@@ -16,24 +18,28 @@ function App() {
   }
   function PAGETHREE (){
     dispatch({ type: 'PAGE_[3]'})
-  }
+  } */
   return (
+    <BrowserRouter>
+      <Routes/>
+    {/* 
     <>
       <Header/>
       <Wallpaper>
+        <lu>
+          {page === "page[0]" ?<></>: <button onClick={PAGEZERO}>PAGE[0]Home</button>}
+          {page === "page[1]" ?<></>:<button onClick={PAGEONE}>PAGE[1]Login</button>}
+          {page === "page[2]" ?<></>:<button onClick={PAGETWO}>PAGE[2]Catalog</button>}
+          {page === "page[3]" ?<></>:<button onClick={PAGETHREE}>PAGE[3]Contato</button>}
+        </lu>
         {page === "page[0]" ? <Home/>:<></>}
         {page === "page[1]" ? <Login/>:<></>}
         {page === "page[2]" ? <Catalog/>:<></>}
         {page === "page[3]" ? <Contato/>:<></>}
-        <lu>
-          <button onClick={PAGEZERO}>PAGE[0]Home</button>
-          <button onClick={PAGEONE}>PAGE[1]Login</button>
-          <button onClick={PAGETWO}>PAGE[2]Catalog</button>
-          <button onClick={PAGETHREE}>PAGE[3]Contato</button>
-        </lu>
+        
       </Wallpaper>
       <Footer/>
-    </>
+    </> */}</BrowserRouter>
   );
 }
 export default App;
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,28 @@
+//import React,{useState, useEffect} from 'react';
+//import {  TopBar, AllPots,BottomBar,Separator, Align, NavTop} from '../../components'
+
+function Homepage() {
+
+    
+  return (
+    <>
+    <h1>
+        Hello Again!
+        ...Again
+        ...Again...
+    </h1>
+    {/* <TopBar/>
+    <Separator y='150'x='150'/>
+    <Align column='column'>
+    <AllPots/>
+    </Align>
+    <Align color='#34033B'>
+     <BottomBar/>
+    </Align>
+    <Separator y='25' />
+    <NavTop/>  */}
+  </>
+  );
+}
+
+export default Homepage;
